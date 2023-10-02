d36dfe95f1e1e452455e2e261e645be9a4794025
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2023-10-02 12:38:01 -0300

    removes regex bug + minor improvements

--- debian11_installer.sh
+++ debian11_installer.sh
@@ -1,4 +1,4 @@
-apt-get -y install gnupg pwgen
+apt-get -y install gnupg pwgen net-tools
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian bullseye/mongodb-org/6.0 main" | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 apt-get -y update && apt-get install -y mongodb-org
@@ -22,12 +22,12 @@ apt-get -y update && apt-get -y install graylog-server
 PASSWORD_SECRET=$(pwgen -N 1 -s 96)
 
 sed -i 's/password_secret =/password_secret = '$PASSWORD_SECRET'/g' /etc/graylog/server/server.conf
-sed -i "s/root_password_sha2 = /root_password_sha2=e7b35bf8870d4b6701dcce6fa652a661149da30eadb1e94b7fdcc830e8af7e52/g" /etc/graylog/server/server.conf
+sed -i "s/root_password_sha2 =/root_password_sha2 = e7b35bf8870d4b6701dcce6fa652a661149da30eadb1e94b7fdcc830e8af7e52/g" /etc/graylog/server/server.conf
 sed -i "s/#http_bind_address = 127.0.0.1:9000/http_bind_address = 0.0.0.0:9000/g" /etc/graylog/server/server.conf
 
 systemctl daemon-reload && systemctl enable graylog-server && systemctl restart graylog-server && systemctl --type=service --state=active | grep graylog-server
 
-CPU_AVX_SUPPORT=$(cat /proc/cpuinfo | grep avxad)
+CPU_AVX_SUPPORT=$(cat /proc/cpuinfo | grep avx)
 if [ -z "$CPU_AVX_SUPPORT" ]; 
 then
  echo ""
