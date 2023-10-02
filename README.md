31277e3c1f9f87b65c3900db74211357b4f384d5
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2023-10-02 12:48:26 -0300

    add improvements

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ git clone https://github.com/wallacemariadeandrade/graylog_installer.git
 cd graylog_installer
 chmod +x debian11_installer.sh
 ./debian11_installer.sh
+
+
+To set "1234" as new admin's password: 
+
+./debian11_installer.sh 1234
 ```
--- debian11_installer.sh
+++ debian11_installer.sh
@@ -21,9 +21,15 @@ apt-get -y update && apt-get -y install graylog-server
 
 PASSWORD_SECRET=$(pwgen -N 1 -s 96)
 
+if [ -z "$1" ]; then
+ sed -i "s/root_password_sha2 =/root_password_sha2 = e7b35bf8870d4b6701dcce6fa652a661149da30eadb1e94b7fdcc830e8af7e52/g" /etc/graylog/server/server.conf
+else 
+ NEW_PASSWORD=$(echo -n $1 | sha256sum | awk {'print $1'})
+ sed -i 's/root_password_sha2 =/root_password_sha2 = '$NEW_PASSWORD'/g' /etc/graylog/server/server.conf
+fi
 sed -i 's/password_secret =/password_secret = '$PASSWORD_SECRET'/g' /etc/graylog/server/server.conf
-sed -i "s/root_password_sha2 =/root_password_sha2 = e7b35bf8870d4b6701dcce6fa652a661149da30eadb1e94b7fdcc830e8af7e52/g" /etc/graylog/server/server.conf
-sed -i "s/#http_bind_address = 127.0.0.1:9000/http_bind_address = 0.0.0.0:9000/g" /etc/graylog/server/server.conf
+sed -i "s/#http_bind_address = 127.0.0.1:9000/http_bind_address = 0.0.0.0:9000/g" /etc/graylog/server/server.conf 
+
 
 systemctl daemon-reload && systemctl enable graylog-server && systemctl restart graylog-server && systemctl --type=service --state=active | grep graylog-server
 
