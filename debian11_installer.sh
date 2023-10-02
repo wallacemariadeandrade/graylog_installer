fe7f3cb52be5995abfe9626c6af419627292b5a0
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2023-10-02 12:25:45 -0300

    add cpu avx check for mongodb

--- debian11_installer.sh
+++ debian11_installer.sh
@@ -21,8 +21,20 @@ apt-get -y update && apt-get -y install graylog-server
 
 PASSWORD_SECRET=$(pwgen -N 1 -s 96)
 
-sed -i 's/password_secret =/password_secret=$PASSWORD_SECRET/g' /etc/graylog/server/server.conf
-sed -i "s/root_password_sha2 =/root_password_sha2=e7b35bf8870d4b6701dcce6fa652a661149da30eadb1e94b7fdcc830e8af7e52/g" /etc/graylog/server/server.conf
+sed -i 's/password_secret =/password_secret = '$PASSWORD_SECRET'/g' /etc/graylog/server/server.conf
+sed -i "s/root_password_sha2 = /root_password_sha2=e7b35bf8870d4b6701dcce6fa652a661149da30eadb1e94b7fdcc830e8af7e52/g" /etc/graylog/server/server.conf
 sed -i "s/#http_bind_address = 127.0.0.1:9000/http_bind_address = 0.0.0.0:9000/g" /etc/graylog/server/server.conf
 
 systemctl daemon-reload && systemctl enable graylog-server && systemctl restart graylog-server && systemctl --type=service --state=active | grep graylog-server
+
+CPU_AVX_SUPPORT=$(cat /proc/cpuinfo | grep avxad)
+if [ -z "$CPU_AVX_SUPPORT" ]; 
+then
+ echo ""
+ echo "========================= WARNING ============================"
+ echo "Graylog's VM CPU currently doesn't support AVX instruction, so MongoDB service won't work."
+ echo "If you're using Proxmox, please go to through your VM Hardware > Processors and"
+ echo "change 'Type' field value to 'Host'"
+ echo "==============================================================="
+ echo ""
+fi
